11b04f327899fc6a55ac229e33bea399b6ce839e
Treinamento <Treinamento@RD723933.raiadrogasil.com>
2020-02-14 14:29:54 -0300

    parte F pronta

---
+++ src/Matricula.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Matricula {
+    private Aluno aluno;
+    private Curso curso;
+    private Date dataMatricula;
+
+    public Matricula(Aluno aluno, Curso curso) {
+        this.aluno = aluno;
+        this.curso = curso;
+        Date date = new Date();
+        this.dataMatricula = date;
+    }
+}
